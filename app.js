71460f85e07f6b6277af0cdb2966605f45294df2
sergiokinc <122182439+sergiokinc@users.noreply.github.com>
2024-07-06 09:57:56 -0500

    Version final

---
+++ app.js
@@ -0,0 +1,79 @@
+let NumeroSecreto = 0;
+let Intentos = 1;
+let ListaNumerosSorteado = [];
+let NumerosMAximo = 10;
+
+console.log (NumeroSecreto)
+
+function AsignarTextoElemento ( elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function VerificarIntento (){
+    let NumeroDeUsuario = parseInt(document.getElementById('ValorUsuario').value);
+
+    if (NumeroDeUsuario === NumeroSecreto) {
+        AsignarTextoElemento('p' , `Acertaste el numero secreto en ${Intentos} ${(Intentos == 1) ? ' vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled')
+        } else {
+            if (NumeroDeUsuario > NumeroSecreto){
+                AsignarTextoElemento ('p', 'El numero es menor')
+            } else {
+            AsignarTextoElemento ('p', 'El numero es mayor')
+            }
+        } Intentos ++;
+        LimpiarCaja();
+
+    return;
+}
+
+
+function LimpiarCaja(){
+   document.querySelector('#ValorUsuario').value= '';
+    
+}
+
+function GenerarNumeroSecreto() {
+    let NumeroGenerado = Math.floor (Math.random()*NumerosMAximo)+1;
+    console.log(NumeroGenerado)
+    console.log(ListaNumerosSorteado)
+// preguntar interno si sorteamos todos los numeros
+    if (ListaNumerosSorteado.length == NumerosMAximo){
+
+        AsignarTextoElemento('p', 'Ya se sortearon los numeros posibles')
+    } else{
+
+        if (ListaNumerosSorteado.includes(NumeroGenerado)){
+            return GenerarNumeroSecreto();
+        }else{
+            ListaNumerosSorteado.push(NumeroGenerado);
+            return NumeroGenerado;
+      
+          }
+    }
+    
+}
+
+function ConceptosIniciales(){
+    AsignarTextoElemento('h1','Juego del numero secretito');
+    AsignarTextoElemento('p',`Indica el numero del a al  ${NumerosMAximo}`);
+    NumeroSecreto = GenerarNumeroSecreto();
+    Intentos = 1;
+
+
+
+}
+
+function ReiniciarJuego(){
+    // limpiar caja
+    LimpiarCaja ();
+    // mensajes iniciales
+    ConceptosIniciales();
+    
+    document.querySelector('#reiniciar').setAttribute('disabled','true')
+
+
+}
+ ConceptosIniciales();
